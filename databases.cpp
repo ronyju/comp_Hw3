29cc9c27e8f9a06dc535995f1ca0d2b639b496e7
Rony Juliusburger <80697658+ronyju@users.noreply.github.com>
2023-05-24 13:34:59 +0300

    Add files via upload
    offset for function arguments.
    functions to check errors

--- databases.cpp
+++ databases.cpp
@@ -8,9 +8,9 @@ string Type::ToString() {
         if (IsOverride()) { type_to_string += " with override, "; }
         type_to_string+= "with | return type - " + return_type + " | and arguments type: ";
         for (int i =0; i<arguments_types.size()-1; i++) {
-            type_to_string+= arguments_types[i] + ", ";
+            type_to_string+= arguments_names[i] + " : " + arguments_types[i] + ", ";
         }
-        type_to_string+= arguments_types[arguments_types.size()-1] + " |";
+        type_to_string+=  arguments_names[arguments_types.size()-1] + " : " + arguments_types[arguments_types.size()-1] + " |";
         type_to_string+= " ) ";
     }
     else {
@@ -34,6 +34,15 @@ SymbolTableElement* SymbolTable::SearchInTableByName (string name, bool* found) 
     }
     *found = false;
     return NOT_FOUND;
+}
+vector <SymbolTableElement*> SymbolTable::GetAllFuncOfName (string name) {
+    vector <SymbolTableElement*> allFuncs;
+    for (int index = 0; index < table_vector.size(); index++) {
+        if (table_vector[index].GetName() == name) { //found
+            allFuncs.push_back(&table_vector[index]);
+        }
+    }
+    return allFuncs;
 }
 void SymbolTable::PrintTable () {
     cout << "printing table: " <<endl;
@@ -85,52 +94,6 @@ string Scope::ScopeTypeStr() {
     }
     return "error no such scopeType";
 }
-void ScopeStack::AddSymbolToCurrentScope(string name, Type type, int offset) {
-    if (scopes_stack.empty()) { cout<< "ERROR trying to add to current scope when there is no scopes! "; return; }
-    if (!type.IsFunc()) {
-        int current_offset = offset_stack.front();
-        offset_stack.pop_front();
-        offset = current_offset;
-        current_offset ++;
-        offset_stack.push_front(current_offset);
-    } else {
-        offset = NA;
-    }
-    Scope* cur_scope = &scopes_stack.front();
-    cur_scope->AddToScope(name, type, offset);
-}
-void ScopeStack::AddSymbolToCurrentScope(SymbolTableElement element) {
-    if (scopes_stack.empty()) { cout<< "ERROR trying to add to current scope when there is no scopes! "; return; }
-    if (!element.IsFunc()) {
-        int current_offset = offset_stack.front();
-        offset_stack.pop_front();
-        element.SetOffset(current_offset);
-        current_offset ++;
-        offset_stack.push_front(current_offset);
-    } else {
-        element.SetOffset(NA);
-    }
-    Scope* cur_scope = &scopes_stack.front(); // this is a copy!
-    cur_scope->AddToScope(element);
-
-}
-void ScopeStack::PushNewScope(ScopeType scope_type) {
-    // meaning add a new scope with empty table.
-    if (scopes_stack.empty()) {// this is the first scope
-        offset_stack.push_front(0);
-    } else {
-        int cur_offset_in_stack = offset_stack.front();
-        offset_stack.push_back(cur_offset_in_stack);
-    }
-    Scope new_scope (scope_type);
-    scopes_stack.push_front(new_scope);
-
-}
-void ScopeStack::PopScope() {
-    // meaning remove the last scope.
-    scopes_stack.pop_front();
-    offset_stack.pop_front();
-}
 SymbolTableElement* ScopeStack::SearchInAllScopesByName(string name, bool* found) {
     if (scopes_stack.empty()) {
         *found = false;
@@ -149,6 +112,21 @@ SymbolTableElement* ScopeStack::SearchInAllScopesByName(string name, bool* found
     *found = false;
     return NOT_FOUND;
 }
+vector <SymbolTableElement*> ScopeStack::GetAllFuncOfName (string name) {
+    // NOTE: call this only after knowing there is at list one func with the same name
+    vector <SymbolTableElement*> allFuncs;
+    list<Scope>::iterator it;
+    for (it = scopes_stack.begin(); it != scopes_stack.end(); it++) {
+        vector <SymbolTableElement*> currentTableVec;
+        currentTableVec = it->sym_table.GetAllFuncOfName(name);
+        if (currentTableVec.size() > 0) {
+            for (int i = 0; i < currentTableVec.size(); i++) {
+                allFuncs.push_back (currentTableVec[i]);
+            }
+        }
+    }
+    return allFuncs;
+}
 void ScopeStack::printStack() {
     if (scopes_stack.empty()) {
         cout << "stack is empty";
@@ -166,3 +144,158 @@ void ScopeStack::printStack() {
     }
     cout << "************************************************** " << endl;
 }
+ErrorType ScopeStack::checkLegalOverride(SymbolTableElement element) {
+    vector <SymbolTableElement*> allFuncs = GetAllFuncOfName(element.GetName());
+    string newFuncRetType = element.GetType().GetReturnType();
+    vector<string> newFuncArgTypes = element.GetType().GetArgumentsTypes();
+    for (int i = 0; i< allFuncs.size(); i++) {
+        if (newFuncRetType == allFuncs[i]->GetType().GetReturnType()) {
+            if (newFuncArgTypes == allFuncs[i]->GetType().GetArgumentsTypes()) {
+                return ERROR_DEF; // new func is identical to an existing func.
+            }
+        }
+    }
+    return NO_ERROR;
+}
+
+
+// for outside use:
+/*
+void ScopeStack::AddSymbolToCurrentScope(string name, Type type, int offset) {
+    if (scopes_stack.empty()) { cout<< "ERROR trying to add to current scope when there is no scopes! "; return; }
+    if (!type.IsFunc()) {
+        int current_offset = offset_stack.front();
+        offset_stack.pop_front();
+        offset = current_offset;
+        current_offset ++;
+        offset_stack.push_front(current_offset);
+    } else {
+        offset = NA;
+    }
+    Scope* cur_scope = &scopes_stack.front();
+    cur_scope->AddToScope(name, type, offset);
+}*/
+void ScopeStack::AddSymbolToCurrentScope(SymbolTableElement element) {
+    if (scopes_stack.empty()) { cout<< "ERROR trying to add to current scope when there is no scopes! "; return; }
+    if (!element.IsFunc()) {
+        int current_offset = offset_stack.front();
+        offset_stack.pop_front();
+        element.SetOffset(current_offset);
+        current_offset ++;
+        offset_stack.push_front(current_offset);
+    } else {
+        element.SetOffset(NA);
+    }
+    Scope* cur_scope = &scopes_stack.front(); // this is a copy!
+    cur_scope->AddToScope(element);
+
+    if (element.IsFunc())   {
+        // add the aruments to the stack as vars:
+        vector<string> argumentsType = element.GetType().GetArgumentsTypes();
+        vector<string> argumentsNames = element.GetType().GetArgumentsNames();
+        for (int i = 0; i<argumentsType.size(); i++) {
+            Type curType (argumentsType[i]);
+            int offset = -1*(i+1);
+            SymbolTableElement curElem (argumentsNames[i], curType, offset);
+            cur_scope->AddToScope(curElem);
+        }
+    }
+}
+
+void ScopeStack::printOffsetStack() {
+    list<int>::iterator it;
+    cout << "printing offset stack: ------- ";
+    for (it = offset_stack.begin(); it != offset_stack.end(); it++) {
+        cout << *it << endl;
+    }
+    cout << "--------------------------------- "<<endl;
+}
+
+void ScopeStack::PushNewScope(ScopeType scope_type) {
+    // meaning add a new scope with empty table.
+    if (scopes_stack.empty()) {// this is the first scope
+        offset_stack.push_front(0);
+    } else {
+        int cur_offset_in_stack = offset_stack.front();
+        offset_stack.push_front(cur_offset_in_stack);
+    }
+    Scope new_scope (scope_type);
+    scopes_stack.push_front(new_scope);
+
+}
+void ScopeStack::PopScope() {
+    // meaning remove the last scope.
+    scopes_stack.pop_front();
+    offset_stack.pop_front();
+}
+// new :
+ErrorType ScopeStack::checkForErrorBeforeAddSymbolToCurrentScope(SymbolTableElement element) {
+    bool found;
+    if (element.IsFunc()) {
+        SymbolTableElement* onefuncInStack;
+        onefuncInStack = SearchInAllScopesByName (element.GetName(), &found);
+        if (found) {
+            if (element.GetType().IsOverride()) { // new func is override
+                // the added func is override, and there is a function with the same name in the stack
+                if (!onefuncInStack->GetType().IsOverride()) { // old func is NOT override
+                    return ERROR_FUNC_NO_OVERRIDE; // trying to run-over a function the is not override.
+                } else { // old func is override
+                    return checkLegalOverride(element);
+                }
+            } else {  // new func is NOT override
+                if (!onefuncInStack->GetType().IsOverride()) {  // old func is NOT override
+                    return ERROR_FUNC_NO_OVERRIDE; // trying to run-over a function the is not override.
+                }
+                else { // old func is override
+                    return ERROR_OVERRIDE_WITHOUT_DECLARATION; // trying to override a function that is override without using override in the declaration
+                }
+            }
+        }
+    } else { // not a func just a var
+        SymbolTableElement* oldVar;
+        oldVar = SearchInAllScopesByName (element.GetName(), &found);
+        if (found) {
+            return ERROR_DEF; // val already exist 
+        }
+    }
+
+    return NO_ERROR;
+}
+ErrorType ScopeStack::checkAfterCallIfFuncExist(string expectedFuncName, vector<string> expectedArgumentsTypes, string expectedReturnType ) {
+    bool found;
+    SymbolTableElement* func;
+    func = SearchInAllScopesByName (expectedFuncName, &found);
+    if (!found || func == nullptr) {
+        return ERROR_UNDEF_FUNC; // calling a function that was not declared.
+    }
+    if (!func->IsFunc()) {
+        return ERROR_UNDEF_FUNC; // using identifier that is not a func as a func.
+    }
+    if (func->GetType().GetArgumentsTypes() != expectedArgumentsTypes) {
+        return ERROR_PROTOTYPE_MISMATCH; // no maching arguments 
+    }
+    if (expectedReturnType != "dont care") {
+        if (expectedReturnType != func->GetType().GetReturnType()) {
+            return ERROR_MISMATCH; //type don't match
+        }
+    }
+    return NO_ERROR;
+}
+ErrorType ScopeStack::checkIfVarExist(string expectedVarName, string expectedVarType) {
+    bool found;
+    SymbolTableElement* var;
+    var = SearchInAllScopesByName (expectedVarName, &found);
+    if(!found || var == nullptr) {
+        return ERROR_UNDEF; // using a var that was not declared
+    }
+    if (var->IsFunc()) {
+        return ERROR_UNDEF; // using a var that is not a var (a function);
+    }
+    if (expectedVarType != "dont care") {
+        if (expectedVarType != var->GetType().GetTypeName()) {
+            return ERROR_MISMATCH; //type don't match
+        }
+    }
+    return NO_ERROR;
+}
+
--- databases.hpp
+++ databases.hpp
@@ -5,8 +5,18 @@
 //#include "hw3_output.hpp"
 using namespace std;
 typedef enum {WHILE, IF, ELSE, FUNC, REGULAR, GLOBAL} ScopeType;
+
+typedef enum {NO_ERROR, ERROR_LEX, ERROR_SYN,
+              ERROR_UNDEF, ERROR_UNDEF_FUNC, ERROR_DEF,
+              ERROR_PROTOTYPE_MISMATCH, ERROR_MISMATCH,
+              ERROR_UNEXPECTED_BREAK, ERROR_UNEXPECTED_CONTINUE,
+              ERROR_MAIN_MISSING, ERROR_BYTE_TOO_LARGE,
+              ERROR_FUNC_NO_OVERRIDE, ERROR_OVERRIDE_WITHOUT_DECLARATION,
+              ERROR_AMBIGUOUS_CALL} ErrorType;
+
 #define NOT_FOUND nullptr
 #define NA -99999
+#define UNDEF -88888
 /* ------------------------------- Type ------------------------------- */
 class Type {
 protected:
@@ -15,20 +25,22 @@ protected:
     bool isOverride; // true when the function has override in its declaration
     string return_type;
     vector<string> arguments_types;
+    vector<string> arguments_names; // same length as arguments_types!
 
 public:
     // constructor for non-function :
     Type(string type_name) : type_name(type_name), return_type("NULL") , isOverride(false){}
 
     // constructor for a function :
-    Type(string return_type, vector<string> arguments_types, bool isOverride) :
-            type_name("NULL"), return_type(return_type), arguments_types(arguments_types) , isOverride(isOverride) {}
+    Type(string return_type, vector<string> arguments_types,  vector<string> arguments_names,  bool isOverride = false) :
+            type_name("NULL"), return_type(return_type), arguments_types(arguments_types), arguments_names(arguments_names) , isOverride(isOverride) {}
 
     bool IsFunc() { return (return_type != "NULL"); }
     bool IsOverride() { return isOverride; }
     string GetTypeName() { return type_name; }
     string GetReturnType() { return return_type; }
     vector<string> GetArgumentsTypes() { return arguments_types; }
+    vector<string> GetArgumentsNames() { return arguments_names; }
     string ToString();
 };
 /* --------------------------- Symbol Table --------------------------- */
@@ -40,7 +52,7 @@ protected:
 
 public:
     SymbolTableElement(string name, Type type, int offset) : name(name), type(type), offset(offset) {}
-
+    SymbolTableElement(string name, Type type) : name(name), type(type), offset(UNDEF) {}
     string GetName() {return name; }
     Type GetType() { return type; }
     int GetOffset() { return offset; }
@@ -59,7 +71,9 @@ public:
     void PrintTable ();// for debug
     string ToString();
     SymbolTableElement* SearchInTableByName (string name, bool* found); // must check found to see if the return is valid!!
+    vector <SymbolTableElement*> GetAllFuncOfName (string name); //might return empty vac
     //TODO: OREN we will have to think of how to implement the search when its an override function, I leave it open for now
+
 };
 /* -------------------------------- scope --------------------------------- */
 class Scope {
@@ -82,12 +96,21 @@ class ScopeStack {
 
 public:
     list<Scope> scopes_stack;
-    list<int> offset_stack; //TODO: OREN I made sure to update the offset like in the tutorial, and update the var/func BUT I didnt added the aruments as negative offset yet. didnt know when and how it should be done
+    list<int> offset_stack;
     ScopeStack() {} //TODO: OREN for now there is a need to OpenNewScope for the first scope
+    SymbolTableElement* SearchInAllScopesByName(string name, bool* found);
+    vector <SymbolTableElement*> GetAllFuncOfName (string name); //note to use this after knowing there is at list one func with this name in the stack.
+    ErrorType checkLegalOverride(SymbolTableElement element);
+    void printOffsetStack();
+
+    // for outside use:
     void AddSymbolToCurrentScope(SymbolTableElement element);
-    void AddSymbolToCurrentScope(string name, Type type, int offset = -1);
+    void AddSymbolToCurrentScope(string name, Type type, int offset = UNDEF) { SymbolTableElement element (name, type, offset); return AddSymbolToCurrentScope (element); }
     void PushNewScope(ScopeType scope_type);
     void PopScope();
-    SymbolTableElement* SearchInAllScopesByName(string name, bool* found);
+    ErrorType checkForErrorBeforeAddSymbolToCurrentScope(string name, Type type, int offset = UNDEF) { SymbolTableElement element (name, type, offset); return checkForErrorBeforeAddSymbolToCurrentScope (element); }
+    ErrorType checkForErrorBeforeAddSymbolToCurrentScope(SymbolTableElement element); //for both func or variable
+    ErrorType checkAfterCallIfFuncExist(string expectedFuncName, vector<string> expectedArgumentsTypes ,string expectedReturnType = "dont care" );// Oren, if you don't care to check the return type dont add it in the arguments
+    ErrorType checkIfVarExist(string expectedVarName, string expectedVarType = "dont care"); //Oren, if you don't care to check the type don't add it in the arguments
     void printStack(); //for debug
 };
